67360ffab740f787e50066fe4b9ce8b871aea888
WANGWei <wei.wang@etu.unice.fr>
2020-01-30 10:01:32 +0100

    Initial commit

---
+++ src/TUIOTag.js
@@ -0,0 +1,55 @@
+/**
+ * @author Christian Brel <ch.brel@gmail.com>
+ */
+
+import TUIOObject from './TUIOObject'
+
+import { TAG_SOCKETIO_TYPE } from './constants'
+
+/**
+ * Main class to manage TUIOTag.
+ *
+ * @class TUIOTag
+ * @extends TUIOObject
+ */
+class TUIOTag extends TUIOObject {
+  /**
+   * TUIOTag constructor.
+   *
+   * @constructor
+   * @param {string/number} id - TUIOTag's id.
+   * @param {string/number} tagId - TUIOTag's tagId.
+   * @param {string/number} x - TUIOTag's abscissa.
+   * @param {string/number} y - TUIOTag's ordinate.
+   * @param {string/number} angle - TUIOTag's angle.
+   */
+  constructor(id, tagId, x, y, angle) {
+    super(id, x, y)
+    this._tagId = tagId
+    this._angle = angle
+  }
+
+  /**
+   * TUIOTag's tagId getter.
+   *
+   * @returns {string|number} TUIOTag's tagId.
+   */
+  get tagId() { return this._tagId }
+
+  /**
+   * Give the TUIOTag's JSON representation.
+   *
+   * @method toJSON
+   */
+  toJSON() {
+    const objJSON = super.toJSON()
+    return {
+      ...objJSON,
+      id: this._tagId,
+      type: TAG_SOCKETIO_TYPE,
+      angle: this._angle,
+    }
+  }
+}
+
+export default TUIOTag
